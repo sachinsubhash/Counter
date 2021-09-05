d247a3a4c250dfaed2b83b086441b16bcc345b85
Sachin Subhash <sachin.sbsh2@gmail.com>
2021-09-05 11:25:40 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./Header";
-import Home from "./Home";
+import React, { Component } from 'react'
 
-function App() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      count:5
+    }
+  }
+  
+  increment = () => {
+    this.setState ({
+      count: count + 5
+    })
+  }
+  decrement = () {
+    this.setState ({
+      count: count - 5
+    })
+  }
+
+render() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+    <div ClassName="App">
+        <button onClick={this.increment} className="increase">Increment</button>
+        <button onClick={this.decrement} className="decrease">Decrement</button>
+        <h1>Count: {this.state.count}</h1>
     </div>
-  );
-}
-
-export default App;
