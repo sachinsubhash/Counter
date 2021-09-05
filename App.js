87a37ec04a15c693164e60842e2b2bc2c8ee8393
Sachin Subhash <sachin.sbsh2@gmail.com>
2021-09-05 11:28:02 +0400

    Update App.js

--- App.js
+++ App.js
@@ -3,27 +3,31 @@ import React, { Component } from 'react'
 export default class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       count:5
     }
   }
-  
+
   increment = () => {
     this.setState ({
-      count: count + 5
-    })
-  }
-  decrement = () => {
-    this.setState ({
-      count: count - 5
-    })
+      count: this.state.count + 5
+    });
   }
 
-render() {
-  return (
-    <div ClassName="App">
-        <button onClick={this.increment} className="increase">Increment</button>
-        <button onClick={this.decrement} className="decrease">Decrement</button>
+  decrement = () => {
+    this.setState ({
+      count: this.state.count - 5
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.increment} className="increaseValue">Increase</button>
+        <button onClick={this.decrement} className="decreaseValue">Decrease</button>
         <h1>Count: {this.state.count}</h1>
-    </div>
+      </div>
+    )
+  }
+}
