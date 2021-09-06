3ed14e6d5e3b8777f9f6ddbaf0c32e8a016d5007
Sachin Subhash <sachin.sbsh2@gmail.com>
2021-09-06 13:16:00 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,23 @@ export default class App extends Component {
     }
   }
 
-  increment() {
+  increment = () => {
     this.setState ({
       count: this.state.count + 5
     })
+  }
+
+  decrement = () => {
+      this.setState ({
+          count: this.state.count - 5
+      })
   }
 
   render() {
     return (
       <div className="App">
         <button onClick={this.increment} className="increasenumber">Increase</button>
+        <button onClick={this.decrement} className="decreasenumber">Decrease</button>
         <h1>Count: {this.state.count}</h1>
       </div>
     )
